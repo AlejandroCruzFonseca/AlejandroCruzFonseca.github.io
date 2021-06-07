fb4deb2eb73329ca8a969927831d6dd72119ed01
alexCF <a.cruz.fons@gmail.com>
2021-06-06 21:28:39 -0700

    Add react components.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { hot } from "react-hot-loader";
+
+import "./App.css";
+
+const App = () => (
+  <div className="App">
+    <h1> Welcome to Ocular React! </h1>
+  </div>
+);
+
+export default hot(module)(App);
